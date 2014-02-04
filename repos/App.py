7fb12afd0de4d5c2f14d892bdafccc3ca644ca1f
Von Welch <von@vwelch.com>
2014-02-04 16:54:54 -0500

    App.py: Override interactive mode if no command given with 'check' command

--- repos/App.py
+++ repos/App.py
@@ -12,12 +12,13 @@ class ReposApp(App):
 
     log = logging.getLogger(__name__)
 
-    def __init__(self):
+    def __init__(self, default_command="check"):
         super(ReposApp, self).__init__(
             description='repos: monitor git repositories',
             version='0.1',
             command_manager=CommandManager('repos'),
         )
+        self.default_command = default_command
 
     def build_option_parser(self, description, version, argparse_kwargs=None):
         self.log.debug('build_option_parser')
@@ -38,6 +39,13 @@ class ReposApp(App):
         self.log.debug(
             "Loading repos from {}".format(self.options.repos_file))
         cmd.store = ReposStore(self.options.repos_file)
+
+    def interact(self):
+        """Action taken if no command given"""
+        self.log.debug("No command given, running default command: %s",
+                       self.default_command)
+        result = self.run_subcommand([self.default_command])
+        return result
 
     def clean_up(self, cmd, result, err):
         self.log.debug('clean_up %s', cmd.__class__.__name__)
